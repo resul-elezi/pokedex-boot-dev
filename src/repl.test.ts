af219bb17ce8689b8964f3cc738bfb04175cf792
Resul Elezi <resul_elezi@protonmail.com>
2025-09-08 17:26:31 +0200

    #

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -6,16 +6,28 @@ describe.each([
         input: "  hello  world  ",
         expected: ["hello", "world"],
     },
-    // TODO: more test cases here
+    {
+        input: "foo   bar   baz",
+        expected: ["foo", "bar", "baz"],
+    },
+    {
+        input: "   one",
+        expected: ["one"],
+    },
+    {
+        input: "two   ",
+        expected: ["two"],
+    },
+    {
+        input: "",
+        expected: [],
+    },
 ])("cleanInput($input)", ({ input, expected }) => {
     test(`Expected: ${expected}`, () => {
-        // TODO: call cleanInput with the input here
+        const actual = cleanInput(input);
 
-        // The `expect` and `toHaveLength` functions are from vitest
-        // they will fail the test if the condition is not met
         expect(actual).toHaveLength(expected.length);
         for (const i in expected) {
-            // likewise, the `toBe` function will fail the test if the values are not equal
             expect(actual[i]).toBe(expected[i]);
         }
     });
