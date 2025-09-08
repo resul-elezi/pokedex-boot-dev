1c467b11137ec76226e7b5b0e5f24e7a3bb4103b
Resul Elezi <resul_elezi@protonmail.com>
2025-09-08 18:02:01 +0200

    #

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,32 @@
+import readline from "readline";
+
 export function cleanInput(input: string): string[] {
     return input
         .trim()
         .split(/\s+/)
         .filter(Boolean);
+}
+
+export function startREPL() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+
+    rl.prompt();
+
+    rl.on("line", (line) => {
+        const words = cleanInput(line);
+
+        if (words.length === 0) {
+            rl.prompt();
+            return;
+        }
+
+        const firstWord = words[0].toLowerCase();
+        console.log(`Your command was: ${firstWord}`);
+
+        rl.prompt();
+    });
 }
